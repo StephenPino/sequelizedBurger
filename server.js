ea5613ac901f883fd3a6578ff775be56d3824373
Stephen Pino <spino725@gmail.com>
2017-05-06 19:59:41 -0500

    change server.js

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static directory
 app.use(express.static(process.cwd() + '/public'));
